51997811ec15c42d4a4d168a8d47272d5b4082d7
Benoit KUGLER <x.ben.x@free.fr>
2018-12-28 22:45:50 +0100

    working on xavier character

--- lua/DB/competences_spe.lua
+++ lua/DB/competences_spe.lua
@@ -1,3 +1,5 @@
+-- In descriptions string the pattern | | will be replace by the color of the skill.
+
 local _ = wesnoth.textdomain "wesnoth-A_Tale_of_Sand_and_Snow"
 
 local apply={} 
@@ -9,13 +11,97 @@ info["rymor"]={}
 
 -- DRUMAR
 
-info["drumar"]={help_des=_"Several years of battles in Vranken company have made Frä Drümar more warlike than any other Frä. She excels at slowing enemies and taking advantage of their delayed reactions.\nShe will earn experience (scaling with enemies level) when applying <span  font_weight ='bold' >slows</span>, <span  font_weight ='bold' >snares</span> or <span  font_weight ='bold' >chilling</span> states to her targets, as well as using <span  font_weight ='bold' >cold</span> attacks.",help_ratios=_"(<span weight ='bold' >x 1</span> per cold attack, <span weight ='bold' >x 1.5</span> per slow, <span weight ='bold' >x 2</span> per snare, <span weight ='bold' >x 2.5</span> per chilling state)",{img="comp_spe/iceball_red.png",max_lvl=3,name="wave_dmg",color='#D02300', name_aff=_"|Cold Strengh :|",require_lvl=4,{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=50,des=_"Grants |10%| bonus damage on the chilling wave. "},{cout_suivant=60,des=_"Grants |20%| bonus damage on the chilling wave. "},{cout_suivant=0,des=_"Grants |one bonus attack| on the chilling wave. "}},{img="comp_spe/forecast.png",max_lvl=3,name="defense",color='#265690',name_aff=_"|Forecast :|",require_lvl=4,{cout_suivant=70,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=70,des=_"Ennemies struggle to hit : |7%| bonus defense. "},{cout_suivant=70,des=_"Ennemies struggle to hit : |14%| bonus defense. "},{cout_suivant=0,des="Ennemies struggle to hit : |21%| bonus defense. "}},{img="comp_spe/slow_zone.png",max_lvl=1,name="slow_zone",color='#00A8A2',name_aff=_"|Slowing field :|",require_avancement={id="toile_atk",des=_"Requires <span weight='bold' color='#919191'>faster entangle</span> "},require_lvl=4,{cout_suivant=110,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=35,des=_"Entangle now |slows all enemies| near the target. "}},{img="attacks/chilling_touch_master.png",max_lvl=3,name="bonus_cold_mistress",color='#1ED9D0',name_aff=_"|Cold Expert :|",require_avancement={id="attack_chilled",des=_"Requires <span weight='bold' color='#1ED9D0'>Chilling touch</span> advancement"},require_lvl=6,{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=35,des=_"Increases Chilling bonus damage to |90%|. "},{cout_suivant=40,des=_"Increases Chilling bonus damage to |110%|. "},{cout_suivant=0,des=_"Chilling state now lasts |3 turns|. "}}}
+info["drumar"]={
+	help_des=_"Several years of battles in Vranken company have made Frä Drümar more warlike than any other Frä. " .. 
+	"She excels at slowing enemies and taking advantage of their delayed reactions. " .. 
+	"\nShe will earn experience (scaling with enemies level) when applying " .. 
+	"<span  font_weight ='bold' >slows</span>, <span  font_weight ='bold' >snares</span> or " .. 
+	" <span  font_weight ='bold' >chilling</span> states to her targets, " .. 
+	"as well as using <span  font_weight ='bold' >cold</span> attacks.",
+	help_ratios=_"(<span weight ='bold' >x 1</span> per cold attack, <span weight ='bold' >x 1.5</span> " ..
+	"per slow, <span weight ='bold' >x 2</span> per snare, <span weight ='bold' >x 2.5</span> per chilling state)",
+	{
+		img="comp_spe/iceball_red.png",max_lvl=3,name="wave_dmg",color='#D02300', 
+		name_aff=_"|Cold Strengh :|",require_lvl=4,
+		{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=50,des=_"Grants |10%| bonus damage on the chilling wave. "},
+		{cout_suivant=60,des=_"Grants |20%| bonus damage on the chilling wave. "},
+		{cout_suivant=0,des=_"Grants |one bonus attack| on the chilling wave. "}
+	},
+	{
+		img="comp_spe/forecast.png",max_lvl=3,name="defense",color='#265690',
+		name_aff=_"|Forecast :|",require_lvl=4,
+		{cout_suivant=70,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=70,des=_"Ennemies struggle to hit : |7%| bonus defense. "},
+		{cout_suivant=70,des=_"Ennemies struggle to hit : |14%| bonus defense. "},
+		{cout_suivant=0,des="Ennemies struggle to hit : |21%| bonus defense. "}
+	},
+	{
+		img="comp_spe/slow_zone.png",max_lvl=1,name="slow_zone",color='#00A8A2',
+		name_aff=_"|Slowing field :|",
+		require_avancement={id="toile_atk",des=_"Requires <span weight='bold' color='#919191'>faster entangle</span> "},
+		require_lvl=4,
+		{cout_suivant=110,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=35,des=_"Entangle now |slows all enemies| near the target. "}
+	},
+	{
+		img="attacks/chilling_touch_master.png",max_lvl=3,name="bonus_cold_mistress",color='#1ED9D0',
+		name_aff=_"|Cold Expert :|",
+		require_avancement={id="attack_chilled",des=_"Requires <span weight='bold' color='#1ED9D0'>Chilling touch</span> advancement"},
+		require_lvl=6,
+		{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=35,des=_"Increases Chilling bonus damage to |90%|. "},
+		{cout_suivant=40,des=_"Increases Chilling bonus damage to |110%|. "},
+		{cout_suivant=0,des=_"Chilling state now lasts |3 turns|. "}
+	}
+}
 
 
 
 -- VRANKEN
 
-info["vranken"]={help_des=_"Vranken has a familly link with his sword. Every time <span  font_weight ='bold' >Göndhul fights</span>, Vranken earn points (scaling with opponents level).",help_ratios=_"(<span weight ='bold'>x 1</span> per defense, <span weight ='bold'>x 2 </span>per attack, <span weight ='bold' >x 3</span> per kill, <span weight ='bold' >10</span> per level up)",{img="comp_spe/leeches_cac.png",max_lvl=3,name="leeches_cac",color='#24BE13',name_aff=_"|Leeches :|",require_lvl=4,{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=35,des=_"Leeches |10%| of damage dealt with swords "},{cout_suivant=40,des=_"Leeches |15%| of damage dealt with swords "},{cout_suivant=0,des=_"Leeches |20%| of damage dealt with swords "}},{img="comp_spe/drain.png",max_lvl=3,name="drain_cac",color='#24BE13',name_aff=_"|Drain :|",require_lvl=4,{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=35,des=_"Drains |30%| of damage dealt with swords, if Göndhul is at most <span weight ='bold'>4</span> hexes away from Vranken "},{cout_suivant=35,des=_"Drains |40%| of damage dealt with swords, if Göndhul is at most <span weight ='bold'>8</span> hexes away from Vranken "},{cout_suivant=0,des=_"Drains |50%| of damage dealt with swords, if Göndhul is at most <span weight ='bold'>16</span> hexes away from Vranken "}},{img="attacks/atk_brut.png",max_lvl=3,name="atk_brut",color='#e7cfa9',name_aff=_"|Lightning sword :|",require_lvl=5,{cout_suivant=100,des=_"<span style='italic'>Skill not learned yet.</span> "},{cout_suivant=50,des=_"Grants a sword which deals |true damage|, but deals |70%| of your usual sword damage "},{cout_suivant=50,des=_"Now deals |85%| of your usual sword damage "},{cout_suivant=0,des=_"Now deals |100%| of your usual sword damage "}},{img="comp_spe/transposition.png",max_lvl=2,name="transposition",color='#9E25C7',name_aff=_"<span style='italic'>|War link|</span>",require_lvl=6,{cout_suivant=150,des=_"<span style='italic'>Skill not learned yet.</span>"},{cout_suivant=100,des=_"Enables Vranken and Göndhul to |switch position|. Cooldown : <span weight='bold'>2</span> turns"},{cout_suivant=0,des=_"Cooldown decreases to <span weight='bold'>1</span>  turn "}}}
+info["vranken"]={
+	help_des=_ "Vranken has a familly link with his sword. ".. 
+	"Every time <span  font_weight ='bold' >Göndhul fights</span>, Vranken earn points " .. 
+	"(scaling with opponents level).",
+	help_ratios= _ "(<span weight ='bold'>x 1</span> per defense, <span weight ='bold'>x 2 </span>per attack, ".. 
+	"<span weight ='bold' >x 3</span> per kill, <span weight ='bold' >10</span> per level up)",
+	{
+		img="comp_spe/leeches_cac.png",max_lvl=3,name="leeches_cac",color='#24BE13',
+		name_aff=_"|Leeches :|",require_lvl=4,
+		{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=35,des=_"Leeches |10%| of damage dealt with swords "},
+		{cout_suivant=40,des=_"Leeches |15%| of damage dealt with swords "},
+		{cout_suivant=0,des=_"Leeches |20%| of damage dealt with swords "}
+	},
+	{
+		img="comp_spe/drain.png",max_lvl=3,name="drain_cac",color='#24BE13',
+		name_aff=_"|Drain :|",require_lvl=4,
+		{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=35,des=_"Drains |30%| of damage dealt with swords, " .. 
+		"if Göndhul is at most <span weight ='bold'>4</span> hexes away from Vranken "},
+		{cout_suivant=35,des=_"Drains |40%| of damage dealt with swords, " .. 
+		"if Göndhul is at most <span weight ='bold'>8</span> hexes away from Vranken "},
+		{cout_suivant=0,des=_"Drains |50%| of damage dealt with swords, ".. 
+		"if Göndhul is at most <span weight ='bold'>16</span> hexes away from Vranken "}
+	},
+	{
+		img="attacks/atk_brut.png",max_lvl=3,name="atk_brut",color='#e7cfa9',
+		name_aff=_"|Lightning sword :|",require_lvl=5,
+		{cout_suivant=100,des=_"<span style='italic'>Skill not learned yet.</span> "},
+		{cout_suivant=50,des=_"Grants a sword which deals |true damage|, but deals |70%| of your usual sword damage "},
+		{cout_suivant=50,des=_"Now deals |85%| of your usual sword damage "},
+		{cout_suivant=0,des=_"Now deals |100%| of your usual sword damage "}
+	},
+	{
+		img="comp_spe/transposition.png",max_lvl=2,name="transposition",color='#9E25C7',
+		name_aff=_"<span style='italic'>|War link|</span>",require_lvl=6,
+		{cout_suivant=150,des=_"<span style='italic'>Skill not learned yet.</span>"},
+		{cout_suivant=100,des=_"Enables Vranken and Göndhul to |switch position|. " .. 
+		"Cooldown : <span weight='bold'>2</span> turns"},
+		{cout_suivant=0,des=_"Cooldown decreases to <span weight='bold'>1</span>  turn "}
+	}
+}
 
 
 function apply.leeches_cac (lvl,unit)
@@ -92,9 +178,44 @@ end
 
 -- BRINX
 
-
-
-info["brinx"]={help_des=_"Brinx seeks to avenge Jödumur's death. Every time he <span  font_weight ='bold' >fights against muspellians</span>, Brinx earn points (scaling with opponents level).",help_ratios=_"(<span   font_weight ='bold' >x 1</span> per defense, <span  font_weight ='bold'>x 3 </span>per attack, <span  font_weight ='bold' >x 5</span> per kill)",{img="icons/armor_leather.png",max_lvl=3,name="def_muspell",color='#4575AE',name_aff=_"|Muspell equilibrium : |",require_lvl=4,{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet </span>"},{cout_suivant=20,des=_"Gives Brinx |10%| defense bonus against muspellians "},{cout_suivant=30,des=_"Gives Brinx |15%| defense bonus against muspellians "},{cout_suivant=0,des=_"Gives Brinx |20%| defense bonus against muspellians "}},{img="icons/crossed_sword_and_hammer.png",max_lvl=3,name="dmg_muspell",color='#D02300',name_aff=_"|Muspell Terror : |",require_lvl=4,{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet </span>"},{cout_suivant=35,des=_"Gives Brinx |10%| bonus damage against Muspell troops "},{cout_suivant=45,des=_"Gives Brinx |20%| bonus damage against Muspell troops "},{cout_suivant=0,des=_"Gives Brinx |30%| bonus damage against Muspell troops "}},{img="icons/potion_red_medium.png",max_lvl=3,name="fresh_blood_musp",color='#24BE13',name_aff=_"|Muspell strength : |",require_lvl=5,{cout_suivant=70,des=_"<span style='italic'>Skill not learned yet</span> "},{cout_suivant=50,des=_"Regenerates |10 HP| when killing a muspellian unit "},{cout_suivant=65,des=_"Regenerates |20 HP| when killing a muspellian unit "},{cout_suivant=0,des=_"Regenerates |30 HP| when killing a muspellian unit "}},{img="comp_spe/bloody_sword.png",max_lvl=2,name="muspell_rage",color='#D02300',name_aff=_"<span style='italic' >|Revenge |</span>",require_lvl=6,{cout_suivant=200,des=_"<span style='italic'>Skill not learned yet </span>"},{cout_suivant=100,des=_"If a muspellian is at his sides, Brinx will take and deal |10%| bonus damage against all enemies "},{cout_suivant=0,des=_"If a muspellian is at his sides, Brinx will take and deal |20%| bonus damage against all enemies "}}}
+info["brinx"]={
+	help_des=_"Brinx seeks to avenge Jödumur's death. Every time he " .. 
+	"<span  font_weight ='bold' >fights against muspellians</span>, " .. 
+	"Brinx earn points (scaling with opponents level).",
+	help_ratios=_"(<span   font_weight ='bold' >x 1</span> per defense, " .. 
+	"<span  font_weight ='bold'>x 3 </span>per attack, <span  font_weight ='bold' >x 5</span> per kill)",
+	{
+		img="icons/armor_leather.png",max_lvl=3,name="def_muspell",color='#4575AE',
+		name_aff=_"|Muspell Equilibrium : |",require_lvl=4,
+		{cout_suivant=50,des=_"<span style='italic'>Skill not learned yet </span>"},
+		{cout_suivant=20,des=_"Gives Brinx |10%| defense bonus against muspellians "},
+		{cout_suivant=30,des=_"Gives Brinx |15%| defense bonus against muspellians "},
+		{cout_suivant=0,des=_"Gives Brinx |20%| defense bonus against muspellians "}
+	},
+	{
+		img="icons/crossed_sword_and_hammer.png",max_lvl=3,name="dmg_muspell",color='#D02300',
+		name_aff=_"|Muspell Terror : |",require_lvl=4,
+		{cout_suivant=40,des=_"<span style='italic'>Skill not learned yet </span>"},
+		{cout_suivant=35,des=_"Gives Brinx |10%| bonus damage against Muspell troops "},
+		{cout_suivant=45,des=_"Gives Brinx |20%| bonus damage against Muspell troops "},
+		{cout_suivant=0,des=_"Gives Brinx |30%| bonus damage against Muspell troops "}
+	},
+	{
+		img="icons/potion_red_medium.png",max_lvl=3,name="fresh_blood_musp",color='#24BE13',
+		name_aff=_"|Muspell Strength : |",require_lvl=5,
+		{cout_suivant=70,des=_"<span style='italic'>Skill not learned yet</span> "},
+		{cout_suivant=50,des=_"Regenerates |10 HP| when killing a muspellian unit "},
+		{cout_suivant=65,des=_"Regenerates |20 HP| when killing a muspellian unit "},
+		{cout_suivant=0,des=_"Regenerates |30 HP| when killing a muspellian unit "}
+	},
+	{
+		img="comp_spe/bloody_sword.png",max_lvl=2,name="muspell_rage",color='#D02300',
+		name_aff=_"<span style='italic' >|Revenge |</span>",require_lvl=6,
+		{cout_suivant=200,des=_"<span style='italic'>Skill not learned yet </span>"},
+		{cout_suivant=100,des=_"If a muspellian is at his sides, Brinx will take and deal |10%| bonus damage against all enemies "},
+		{cout_suivant=0,des=_"If a muspellian is at his sides, Brinx will take and deal |20%| bonus damage against all enemies "}
+	}
+}
 
 
 function apply.def_muspell (lvl,unit)
--- lua/DB/heros.lua
+++ lua/DB/heros.lua
@@ -2,74 +2,81 @@
 HE={}
 
 
-local heros = {}
+local heroes = {}
 
---Heros  vranken 
-heros["vranken"] ={variables = {}}
-heros.vranken.variables.xp =  0
-heros.vranken.variables.bloodlust = false
-heros.vranken.variables.table_comp_spe = {leeches_cac=0, drain_cac=0, atk_brut=0, transposition=0}
-heros.vranken.variables.comp_spe = false
-heros.vranken.variables.comp_spe_lvl = 1
-heros.vranken.variables.comp_spe_cd = 0
+--Heroes  vranken 
+heroes["vranken"] ={variables = {}}
+heroes.vranken.variables.xp =  0
+heroes.vranken.variables.bloodlust = false
+heroes.vranken.variables.table_comp_spe = {leeches_cac=0, drain_cac=0, atk_brut=0, transposition=0}
+heroes.vranken.variables.comp_spe = false
+heroes.vranken.variables.comp_spe_lvl = 1
+heroes.vranken.variables.comp_spe_cd = 0
 
---Heros  rymor 
-heros["rymor"] ={variables = {}} 
-heros.rymor.variables.xp = 0
-heros.rymor.variables.bloodlust = false
-heros.rymor.variables.table_comp_spe = {}
-heros.rymor.variables.comp_spe = false
-heros.rymor.variables.comp_spe_lvl = 1
-heros.rymor.variables.comp_spe_cd = 0
-heros.rymor.color = '#BCB4D6'
+--Heroes  rymor 
+heroes["rymor"] ={variables = {}} 
+heroes.rymor.variables.xp = 0
+heroes.rymor.variables.bloodlust = false
+heroes.rymor.variables.table_comp_spe = {}
+heroes.rymor.variables.comp_spe = false
+heroes.rymor.variables.comp_spe_lvl = 1
+heroes.rymor.variables.comp_spe_cd = 0
+heroes.rymor.color = '#BCB4D6'
 
---Heros  drumar 
-heros["drumar"] ={variables = {}}
-heros.drumar.variables.xp =  0
-heros.drumar.variables.bloodlust = false
-heros.drumar.variables.table_comp_spe ={ wave_dmg = 0 , defense =0 , slow_zone=0 , bonus_cold_mistress=0}
-heros.drumar.variables.comp_spe = false
-heros.drumar.color = "#00FFF5"
+--Heroes  drumar 
+heroes["drumar"] ={variables = {}}
+heroes.drumar.variables.xp =  0
+heroes.drumar.variables.bloodlust = false
+heroes.drumar.variables.table_comp_spe ={ wave_dmg = 0 , defense =0 , slow_zone=0 , bonus_cold_mistress=0}
+heroes.drumar.variables.comp_spe = false
+heroes.drumar.color = "#00FFF5"
 
---Heros  bunshop 
-heros["bunshop"] ={variables = {}}  
-heros.bunshop.variables.xp =  0
-heros.bunshop.variables.bloodlust = false
-heros.bunshop.variables.table_comp_spe = {}
-heros.bunshop.variables.comp_spe = false
-heros.bunshop.variables.comp_spe_lvl = 1
-heros.bunshop.variables.comp_spe_cd = 0
-heros.bunshop.color = '#FFFA80'
+--Heroes  bunshop 
+heroes["bunshop"] ={variables = {}}  
+heroes.bunshop.variables.xp =  0
+heroes.bunshop.variables.bloodlust = false
+heroes.bunshop.variables.table_comp_spe = {}
+heroes.bunshop.variables.comp_spe = false
+heroes.bunshop.variables.comp_spe_lvl = 1
+heroes.bunshop.variables.comp_spe_cd = 0
+heroes.bunshop.color = '#FFFA80'
 
---Heros  morgane 
-heros["morgane"] = {variables ={}} 
-heros.morgane.variables.xp =  0
-heros.morgane.variables.bloodlust = false
-heros.morgane.variables.table_comp_spe = {}
-heros.morgane.variables.comp_spe = false
-heros.morgane.variables.comp_spe_lvl = 1
-heros.morgane.variables.comp_spe_cd = 0
+--Heroes  morgane 
+heroes["morgane"] = {variables ={}} 
+heroes.morgane.variables.xp =  0
+heroes.morgane.variables.bloodlust = false
+heroes.morgane.variables.table_comp_spe = {}
+heroes.morgane.variables.comp_spe = false
+heroes.morgane.variables.comp_spe_lvl = 1
+heroes.morgane.variables.comp_spe_cd = 0
 
---Heros  brinx 
-heros["brinx"] = {variables ={}} 
-heros.brinx.variables.xp =  0
-heros.brinx.variables.bloodlust = false
-heros.brinx.variables.table_comp_spe = {def_muspell = 0 , dmg_muspell =0 , fresh_blood_musp=0 , muspell_rage=0}
-heros.brinx.variables.comp_spe = false
-heros.brinx.color = '#357815'
+--Heroes  brinx 
+heroes["brinx"] = {variables ={}} 
+heroes.brinx.variables.xp =  0
+heroes.brinx.variables.bloodlust = false
+heroes.brinx.variables.table_comp_spe = {def_muspell = 0 , dmg_muspell =0 , fresh_blood_musp=0 , muspell_rage=0}
+heroes.brinx.variables.comp_spe = false
+heroes.brinx.color = '#357815'
 
 
---Heros  sword_spirit 
-heros["sword_spirit"] = {variables ={}}  
-heros.sword_spirit.variables.bloodlust = false
-heros.sword_spirit.variables.table_comp_spe = {}
-heros.sword_spirit.variables.comp_spe = false
-heros.sword_spirit.color = "#A00E27"
+--Heroes  sword_spirit 
+heroes["sword_spirit"] = {variables ={}}  
+heroes.sword_spirit.variables.bloodlust = false
+heroes.sword_spirit.variables.table_comp_spe = {}
+heroes.sword_spirit.variables.comp_spe = false
+heroes.sword_spirit.color = "#A00E27"
+
+-- Heroes Xavier
+heroes["xavier"] = {variables = {}}
+heroes.xavier.variables.bloodlust = false
+heroes.xavier.variables.table_comp_spe = {}
+heroes.xavier.variables.comp_spe = false
+heroes.xavier.color = "#a99508"
 
 -- Should be called once, at first use of the hero
 function HE.init(myId)
-	local u = wesnoth.get_unit( myId)
-	local r = heros[myId].variables
+	local u = wesnoth.get_unit(myId)
+	local r = heroes[myId].variables
 	for i,v in pairs(r)  do
 		if u.variables[i] ~= nil then
 			u.variables[i] = r[i]
@@ -79,5 +86,5 @@ end
 
 
 function HE.get_color(myId)
-	return heros[myId].color
+	return heroes[myId].color
 end
