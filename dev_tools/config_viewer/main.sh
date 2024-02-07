4336f3b1ecdec96ae48ed847e8704fa18c577a61
Benoit KUGLER <benoit.kugler@gmail.com>
2024-02-07 10:03:44 +0100

    fix config launcher

--- dev_tools/config_viewer/main.sh
+++ dev_tools/config_viewer/main.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# cd /home/benoit/.config/wesnoth-1.14/data/add-ons/A_Tale_of_Sand_and_Snow/doc_scenario/outils_carac
+python3 launcher.py 
 
-# python3 main.py
-
-# python3 /usr/share/games/wesnoth/1.14/data/tools/wmlindent /home/benoit/.config/wesnoth-1.14/data/add-ons/A_Tale_of_Sand_and_Snow
-# cat
+python3 wmlindent/wmlindent.py ../../units/
 
